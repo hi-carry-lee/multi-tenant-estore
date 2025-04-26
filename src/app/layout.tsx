c709e9decf4e1b46ab9e74367eb58be593730649
liren <327381633@qq.com>
2025-04-27 05:18:43 +0800

    feat: did some customization for Neobrutalism  style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
-
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  variable: "--font-dm-sans",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`${dmSans.className} antialiased`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen p-20">
+    <div className="flex flex-col items-center justify-center h-screen p-20 max-w-2xl gap-4">
       <h1>Hello World</h1>
-      <Button>Click me</Button>
+      <Button variant="elevated">Click me</Button>
+      <Input
+        type="text"
+        placeholder="Enter your name"
+        className="w-full text-base"
+      />
+      <Progress value={33} />
     </div>
   );
 }
